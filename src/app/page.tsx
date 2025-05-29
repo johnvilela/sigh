0d1e95ad329c2b084048e49212ad9e2b4590566e
Joao Vilela <joao.vilela@gsw.com.br>
2025-05-28 23:20:22 -0300

    feat: usar rota / para redirecionar pro login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
-export default function Home() {
+'use client';
+
+import { Heading } from '@/components/ui/heading';
+import { Text } from '@/components/ui/text';
+import Image from 'next/image';
+import { redirect } from 'next/navigation';
+import { useEffect } from 'react';
+
+export default function HomePage () {
+  useEffect(() => {
+    setTimeout(() => {
+      redirect('/login');
+    }, 2000);
+  });
+
   return (
-    <main>
-      <div>Hello world!</div>
+    <main className="grid place-items-center h-screen">
+      <div className="max-w-md flex flex-col items-center">
+        <Image src="/cbhg-logo.png" width={224} height={124} alt="Logo da CBHG" />
+        <Heading>SIGH</Heading>
+        <Text variant="lead">Aguarde um instante para fazer login</Text>
+      </div>
     </main>
   );
 }
