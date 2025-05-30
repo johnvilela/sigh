87b2d9573edda42c6472868bdc22322550956255
Joao Vilela <joao.vilela@gsw.com.br>
2025-05-30 19:23:52 -0300

    feat: criar componente Textarea

---
+++ src/components/ui/textarea.tsx
@@ -0,0 +1,29 @@
+import * as React from "react"
+
+import { classHelper } from "@/lib/utils/class-helper"
+import { Label } from "./label"
+
+interface TextareaProps extends React.ComponentProps<"textarea"> {
+  label?: string;
+  divClassName?: string;
+}
+
+function Textarea ({ className, divClassName, label, name, ...props }: TextareaProps) {
+  return (
+    <div className={classHelper('w-full max-w-sm', divClassName)}>
+      {label && <Label htmlFor={name}>{label}</Label>}
+      <textarea
+        data-slot="textarea"
+        id={name}
+        name={name}
+        className={classHelper(
+          "border-input placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          className
+        )}
+        {...props}
+      />
+    </div>
+  )
+}
+
+export { Textarea }
